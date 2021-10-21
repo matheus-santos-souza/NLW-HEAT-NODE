7fbe2e9021737522296ecb89886bb6ffb4899a94
Matheus-Santos-Souza <matheus.santos.souza2020@outlook.com>
2021-10-21 18:16:22 -0400

    first commit

---
+++ src/controllers/GetLast3MessageController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+import { GetLast3MessageService } from "../services/GetLast3MessageService"
+
+
+class GetLast3MessageController {
+   async handle (request: Request, response: Response) {
+      const service = new GetLast3MessageService()
+
+      const result = await service.execute()
+
+      response.json(result)
+   }
+}
+
+export { GetLast3MessageController }
